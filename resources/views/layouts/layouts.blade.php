660b92e8df192adb61794d69fcc1440a895c1eea
TakanoriShima <quark2galaxy@gmail.com>
2021-08-11 07:33:23 +0000

    layouts.blade.php for https

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -3,7 +3,11 @@
         <title>@yield('title')</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        @if(app('env') == 'production')
+            <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        @else
+            <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        @endif
     </head>
     <body>
         @component('components.header')
@@ -16,5 +20,10 @@
         @endcomponent
 
         <script src="{{ mix('js/app.js') }}"></script>
+        @if(app('env') == 'production')
+            <script src="{{ secure_asset('js/app.js') }}"></script>
+        @else
+            <script src="{{ asset('js/app.js') }}"></script>
+        @endif
     </body>
 </html> 
